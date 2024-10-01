d1839844a1a1d0e00942d541994b497b8b7c0416
Rimfis <dimitri.rim@yandex.ru>
2024-10-01 17:35:01 +0300

    feat: adding file read implementation

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const data = await readFile(pathToFile, { encoding: 'utf8' });
+
+        console.log(data);
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
 await read();
