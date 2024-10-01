65782d56ac46e5c588c9099d6cb9024f4da7e934
Rimfis <dimitri.rim@yandex.ru>
2024-10-01 16:36:17 +0300

    added file creation implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { writeFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const data = 'I am fresh and young';
+    
+    try {
+        await writeFile(join(__dirname, 'files', 'fresh.txt'), data, {flag: 'wx'});
+    } catch (err) {
+        throw Error('FS operation failed');
+    }
 };
 
 await create();
