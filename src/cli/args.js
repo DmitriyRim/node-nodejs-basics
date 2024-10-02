6b3408d4886cc5a39492986831c0b1668a1084cc
Rimfis <dimitri.rim@yandex.ru>
2024-10-02 19:01:41 +0300

    feat: adding args implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    args.forEach(((item, index, arr) => {
+        if (item.match(/^--/)) {
+            console.log(`${item.slice(2)} is ${arr[index + 1]}`)
+        }
+    }))
 };
 
 parseArgs();
