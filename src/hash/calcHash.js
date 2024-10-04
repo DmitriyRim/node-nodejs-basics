7b52c20e7aea2683145335153e58d93bb473b20b
Rimfis <dimitri.rim@yandex.ru>
2024-10-04 15:03:49 +0300

    feat: adding hash implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const input = createReadStream(join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+
+    input.on('readable', () => {
+        const data = input.read();
+        if(data){
+            hash.update(data)
+        } else {
+            console.log(`${hash.digest('hex')}`)
+        }
+    })
 };
 
 await calculateHash();
