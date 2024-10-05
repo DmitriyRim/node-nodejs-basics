a35981f44b14a0720ad5c17bd3cff40d20dd3f9c
Rimfis <dimitri.rim@yandex.ru>
2024-10-05 11:06:20 +0300

    feat: adding streams:write implementation

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'));
+
+    console.log('Please enter the text you would like to write to the file. \nTo exit, please enter "exit".')
+    stdin.on('data', (data) => {
+        if (data.toString().trim() === 'exit'){
+            writeStream.end();
+            stdin.pause();
+            return;
+        }
+
+        writeStream.write(data);
+    });
 };
 
 await write();
