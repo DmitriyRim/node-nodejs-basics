92a1a0ff8a7efaa94ef83b00b292f5c4b50275f2
Rimfis <dimitri.rim@yandex.ru>
2024-10-05 17:07:59 +0300

    feat: adding decompress implementation

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(join(__dirname, 'files', 'archive.gz')),
+        createGunzip(),
+        createWriteStream(join(__dirname, 'files', 'fileToCompress.txt')),
+    )
 };
 
 await decompress();
