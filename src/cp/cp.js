10e7fd2ad66ab3639ad195b0488c4e7e7ec20cc3
Rimfis <dimitri.rim@yandex.ru>
2024-10-07 11:54:38 +0300

    feat: adding child process implementation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const spawnChild = spawn(`node ${join(__dirname, 'files', 'script.js')}`, [...args], { shell: true });
+
+    spawnChild.stdin.write('Hello. I am from main process!');
+
+    spawnChild.stdout.on('data', (data) => {
+        console.log(data.toString())
+    })
+
+    spawnChild.on('error', (data) => {
+        console.error(`stderr: ${data}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['12', '23']);
