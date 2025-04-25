4114f911e8a7ee0144379c8e0f460d6026756993
Rimfis <dimitri.rim@yandex.ru>
2025-04-25 23:29:44 +0300

    feat: add cli/args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.splice(2);
+
+    args.forEach((arg, index) => {
+        if(arg.match(/^--/i)) {
+            console.log(`${arg.slice(2)} is ${args[index + 1]}`);
+        }
+    });
+
 };
 
 parseArgs();
