60b648c243589a638887dd345f1a75b2290f5662
Rimfis <dimitri.rim@yandex.ru>
2025-04-25 22:55:37 +0300

    feat: add read file function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,7 +1,6 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { readdir } from 'node:fs/promises';
-import { access, constants } from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+    const errorMessage = 'FS operation failed';
+    
+    try {
+        const data = await readFile(pathToFile, { encoding: 'utf8' });
+        console.log(data);
+    } catch {
+        throw new Error(errorMessage);
+    }
 };
 
 await read();
