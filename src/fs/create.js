8d14bdfc435ec1302da25e2b169fad5c1ae06c88
Rimfis <dimitri.rim@yandex.ru>
2025-04-25 18:46:20 +0300

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { writeFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const text = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+
+    try {
+       await writeFile(join(__dirname, 'files', 'fresh.txt'), text, { flag: 'wx' });
+    } catch {
+        throw Error(errorMessage);
+    }
 };
 
 await create();
