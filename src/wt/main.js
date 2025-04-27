7646286de2e171fa1acfd4fd930341c2b8ef91ae
Rimfis <dimitri.rim@yandex.ru>
2025-04-27 16:11:14 +0300

    feat: add worker_threads function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from 'node:worker_threads';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import os from 'node:os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus();
+    const results = new Array(cpus.length).fill(0).map((_, index) => {
+        return new Promise((resolve, rejects) => {
+            const worker = new Worker(join(__dirname, 'worker.js'), { workerData: index + 10 });
+            
+            worker.on('message', (data) => resolve({status: 'resolved', data}));
+            worker.on('error', () => rejects({status: 'error', data: null}))
+        })
+    })
+     
+    console.log(await Promise.all(results))
 };
 
 await performCalculations();
