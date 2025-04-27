507e10dc51165865027ab8d454a13ed58e287e7c
Rimfis <dimitri.rim@yandex.ru>
2025-04-27 12:15:30 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, _, callback){
+            const data = chunk.toString().trim();
+            this.push(data.split('').reverse().join('') + '\n');
+            
+            callback();
+        }
+    })
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
