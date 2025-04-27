c1addfc66817a93bbee932f7751f3a9e3428e5a4
Rimfis <dimitri.rim@yandex.ru>
2025-04-27 10:19:06 +0300

    feat: add write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createWriteStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+    const write = createWriteStream(pathToFile);
+
+    process.stdin.pipe(write);
 };
 
 await write();
