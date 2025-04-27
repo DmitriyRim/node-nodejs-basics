f345a8a5f2734d24acace13fcf831e7e8a952522
Rimfis <dimitri.rim@yandex.ru>
2025-04-27 10:13:28 +0300

    feat: add read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+    const read = createReadStream(pathToFile);
+
+    read.pipe(process.stdout);
+    read.on('end', () => {
+        console.log('\n');
+    })
 };
 
 await read();
