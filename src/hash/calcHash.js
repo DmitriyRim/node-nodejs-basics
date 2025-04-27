3953450c138c63694ba0b0387455d49223110be4
Rimfis <dimitri.rim@yandex.ru>
2025-04-27 09:58:01 +0300

    feat: add calchash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const read = createReadStream(pathToFile);
+    const hash = createHash('sha256');
+
+    read.on('readable', () => {
+        const data = read.read();
+        data ? hash.update(data) : console.log('Hash:', hash.digest('hex'));
+    })
 };
 
 await calculateHash();
